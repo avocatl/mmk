c902d5c252adc6913fd71544ab6e6661ee1af301
VictorAvelar <victor@zizooboats.com>
2021-10-25 11:40:29 +0200

    Add missing base files

---
+++ bm/docs.go
@@ -0,0 +1,18 @@
+// Package bm contains the go bindings for MMK's booking manager REST API.
+//
+// Booking Manager API is a standard Web service that enables clients
+// to connect their data with external systems and services and to extend
+// the automation benefits of the Booking Manager System.
+//
+// It is used by charter agencies to publish availabilities of Charter Operators
+// and automate booking processes by connecting Booking Manager to their own web sites
+// to allow for seamless online booking experience for end users or integrating it with
+// their CRM solutions in order to automate office procedures.
+//
+// Charter operators use the web service to connect Booking Manager with their book keeping
+// softwares and save time and effort in accounting department.
+//
+// Third Party Software tools also use Booking Manager API to display richer information,
+// for example boat tracking systems are able to visualise client contact information directly on
+// the map showing the boat location in real time.
+package bm
