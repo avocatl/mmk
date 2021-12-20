4831cef1d9605daa1dcc6f62e2b91928ef896008
Albert Scherman <albert@zizooboats.com>
2021-12-20 16:47:35 +0100

    OPS-321: Add offer request service (#7)
    * OPS-321: Add offer request service
    
    * OPS-321: Changed query param encoding

--- bm/offer.go
+++ bm/offer.go
@@ -1,21 +1,65 @@
 package bm
 
-import "time"
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/google/go-querystring/query"
+	"net/http"
+	"time"
+)
 
 // Offer is a product, price and time frame proposal for a
 // booking operation.
 type Offer struct {
-	YatchID            int64      `json:"yatchId,omitempty"`
+	YachtId            int64      `json:"yachtId,omitempty"`
 	StartBaseID        int64      `json:"startBaseId,omitempty"`
 	EndBaseID          int64      `json:"endBaseId,omitempty"`
 	StartPrice         float64    `json:"startPrice,omitempty"`
 	Price              float64    `json:"price,omitempty"`
 	DiscountPercentage float64    `json:"discountPercentage,omitempty"`
-	Yatch              string     `json:"yatch,omitempty"`
+	Yacht              string     `json:"yacht,omitempty"`
 	StartBase          string     `json:"startBase,omitempty"`
 	EndBase            string     `json:"endBase,omitempty"`
 	Product            string     `json:"product,omitempty"`
 	Currency           string     `json:"currency,omitempty"`
 	DateFrom           *time.Time `json:"dateFrom,omitempty"`
 	DateTo             *time.Time `json:"dateTo,omitempty"`
+}
+
+// OfferOptions are params that could be passed to the offer request
+type OfferOptions struct {
+	DateFrom *time.Time
+	DateTo   *time.Time
+	YachtId  *int64
+}
+
+type OffersService service
+
+// GetOffers returns offers based on passed parameters
+func (os *OffersService) GetOffers(opts *OfferOptions) (or []*Offer, err error) {
+	var target string
+	{
+		target = "offers"
+
+		if opts != nil {
+			v, _ := query.Values(opts)
+			target = fmt.Sprintf("%s?%s", target, v.Encode())
+		}
+	}
+
+	req, err := os.client.NewAPIRequest(http.MethodGet, target, nil)
+	if err != nil {
+		return
+	}
+
+	res, err := os.client.Do(req)
+	if err != nil {
+		return
+	}
+
+	if err = json.Unmarshal(res.content, &or); err != nil {
+		return
+	}
+
+	return
 }
