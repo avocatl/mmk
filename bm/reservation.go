36dc2bfa40cd7ae71ad7d78afca74514bf400df2
Albert Scherman <albert@zizooboats.com>
2022-08-08 12:45:41 +0200

    Made MMKDateTime default

--- bm/reservation.go
+++ bm/reservation.go
@@ -5,24 +5,23 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
-	"time"
 )
 
 // Reservation represents a booking and its links
 // to a yatch and client.
 type Reservation struct {
-	ID             *int64     `json:"ID,omitempty"`
-	DateFrom       *time.Time `json:"dateFrom,omitempty"`
-	DateTo         *time.Time `json:"dateTo,omitempty"`
-	ExpirationDate *time.Time `json:"ExpirationDate,omitempty"`
-	YatchID        int64      `json:"yatchId,omitempty"`
-	Status         int64      `json:"status,omitempty"`
-	ProductName    string     `json:"productName,omitempty"`
-	BaseFromID     int64      `json:"baseFromId,omitempty"`
-	BaseToID       int64      `json:"baseToId,omitempty"`
-	ClientName     string     `json:"clientName,omitempty"`
-	ClientID       int64      `json:"clientId,omitempty"`
-	Currency       string     `json:"currency,omitempty"`
+	ID             *int64       `json:"ID,omitempty"`
+	DateFrom       *MMKDateTime `json:"dateFrom,omitempty"`
+	DateTo         *MMKDateTime `json:"dateTo,omitempty"`
+	ExpirationDate *MMKDateTime `json:"ExpirationDate,omitempty"`
+	YatchID        int64        `json:"yatchId,omitempty"`
+	Status         int64        `json:"status,omitempty"`
+	ProductName    string       `json:"productName,omitempty"`
+	BaseFromID     int64        `json:"baseFromId,omitempty"`
+	BaseToID       int64        `json:"baseToId,omitempty"`
+	ClientName     string       `json:"clientName,omitempty"`
+	ClientID       int64        `json:"clientId,omitempty"`
+	Currency       string       `json:"currency,omitempty"`
 }
 
 // ReservationRequest describes the request to query or update reservations
